f9397a16cbf7e1168c1fad30ca44894a0e340a64
Jon Perry <jpper@umich.edu>
2025-04-23 09:46:17 -0400

    Implement delete and replace functionality on cover image

--- app/api/edgestore/[...edgestore]/route.ts
+++ app/api/edgestore/[...edgestore]/route.ts
@@ -5,13 +5,19 @@ const es = initEdgeStore.create();
  * This is the main router for the EdgeStore buckets.
  */
 const edgeStoreRouter = es.router({
-  publicFiles: es.fileBucket(),
+  publicFiles: es.fileBucket().beforeDelete(({ ctx, fileInfo }) => {
+    // console.log("beforeDelete", ctx.fileInfo);
+    return true;
+  }),
 });
+
 const handler = createEdgeStoreNextHandler({
   router: edgeStoreRouter,
 });
+
 export { handler as GET, handler as POST };
 /**
  * This type is used to create the type-safe client for the frontend.
  */
+
 export type EdgeStoreRouter = typeof edgeStoreRouter;
